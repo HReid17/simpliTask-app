d05cdbd1b9be709e06009f372960f45dea0f8bfd
Harrison Reid <H.Reid17@outlook.com>
2025-11-10 14:11:43 +0000

    removed the css page for DashboardCalendar

--- src/features/calendar/DashboardCalendar.jsx
+++ src/features/calendar/DashboardCalendar.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import "./dashboardCalendar.css"
 
 export default function DashboardCalendar() {
 
